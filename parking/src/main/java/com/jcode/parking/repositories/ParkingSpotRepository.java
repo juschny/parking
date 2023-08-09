c37e9f47e26f292d8e0dd3a41dd5316839e3e7ad
juschny <97036080+juschny@users.noreply.github.com>
2023-08-09 00:40:30 -0300

    Add files via upload

---
+++ parking/src/main/java/com/jcode/parking/repositories/ParkingSpotRepository.java
@@ -0,0 +1,15 @@
+package com.jcode.parking.repositories;
+
+import com.jcode.parking.models.ParkingSpotModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+@Repository
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {
+
+    boolean existsByLicensePlateCar(String licensePlateCar);
+    boolean existsByParkingSpotNumber(String parkingSpotNumber);
+    boolean existsByApartmentAndBlock(String apartment, String block);
+
+}
